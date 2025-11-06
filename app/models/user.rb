66f5c0ed7fccbb541c290da368d1802edfc06959
Paul Jones <pauljones23@gmail.com>
2025-11-06 14:28:57 +1000

    Run authentication generator

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ApplicationRecord
   has_secure_password
 
-  # Relationships
+  has_many :sessions, dependent: :destroy
   has_many :brand_memberships, dependent: :destroy
   has_many :brands, through: :brand_memberships
 
-  # Validations
-  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  normalizes :email_address, with: ->(e) { e.strip.downcase }
+
+  validates :email_address, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :name, presence: true
 end
