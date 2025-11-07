5c0697a4c35c1cf4617fe575e5c527c07b2d4918
Paul Jones <pauljones23@gmail.com>
2025-11-07 11:16:18 +1000

    Calculation mechanism for section progress

--- app/controllers/brand/vision_controller.rb
+++ app/controllers/brand/vision_controller.rb
@@ -1,10 +1,12 @@
 class Brand::VisionController < Brand::BaseController
   def show
     @brand_vision = @brand.brand_vision || @brand.create_brand_vision!
+    @vision_presenter = BrandVisionPresenter.new(@brand_vision)
   end
 
   def update
     @brand_vision = @brand.brand_vision || @brand.create_brand_vision!
+    @vision_presenter = BrandVisionPresenter.new(@brand_vision)
 
     # Parse core_values if they come as JSON strings from JavaScript
     parsed_params = brand_vision_params
@@ -20,7 +22,12 @@ class Brand::VisionController < Brand::BaseController
 
     if @brand_vision.update(parsed_params)
       respond_to do |format|
-        format.turbo_stream { render turbo_stream: turbo_stream.replace("save_indicator", partial: "shared/save_indicator", locals: { saved: true }) }
+        format.turbo_stream do
+          render turbo_stream: [
+            turbo_stream.replace("save_indicator", partial: "shared/save_indicator", locals: { saved: true }),
+            turbo_stream.replace("section_progress", partial: "shared/section_progress", locals: { presenter: @vision_presenter })
+          ]
+        end
         format.html { redirect_to brand_vision_path(@brand), notice: "Brand vision updated successfully." }
         format.json { head :no_content }
       end
